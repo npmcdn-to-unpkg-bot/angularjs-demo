8976c940bad58fd8943c85765d8462d66b47d887
szl <963237028@qq.com>
2016-07-06 22:13:47 +0800

    增加影藏代码

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ mc_app.controller('mc-my-ctr',function($scope){
 	$scope.first_name = 'shi';
 	$scope.send_name = 'zhongliang';
 });
-
+angular.bootstrap(document.getElementById('mc-app'),['mc-app']);
 var expression_number = angular.module('expression_number',[]);
+
